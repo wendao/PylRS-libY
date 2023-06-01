81a3f42f458cabfa2d7c18d6c90d75883bc05a8e
Yuan Liu <wendao@pku.edu.cn>
2023-06-01 15:51:10 +0800

    adding TCOC

--- 4.prediction/fit_TCO.sh
+++ 4.prediction/fit_TCO.sh
@@ -6,9 +6,9 @@ do
     #K
     #echo pair_fit TCO-gaff-${1} and name O13+C14+C21, ${f} and id 14+16+18
     #Y
-    echo pair_fit TCO-gaff-${1} and name O13+C14+C21, ${f} and id 15+16+18
-    #C
-    #echo pair_fit TCO-gaff-${1} and name O13+C14+C21, ${f} and id 14+15+17
+    #echo pair_fit TCO-gaff-${1} and name O13+C14+C21, ${f} and id 15+16+18
+    #C/S
+    echo pair_fit TCO-gaff-${1} and name O13+C14+C21, ${f} and id 14+15+17
 
     echo save fit/${f}.pdb, TCO-gaff-${1}
     echo delete ${f}
